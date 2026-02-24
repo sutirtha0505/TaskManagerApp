9f7f460112d3c0b5a25d390e517e0006d1a99bb0
Tirtharaj Karmakar <tirtharajkarmakarinlinux@gmail.com>
2026-02-24 13:02:34 +0530

    Initial Commit

---
+++ TaskManagerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskManagerApp
+//
+//  Created by Tirtharaj Karmakar on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by Tirtharaj Karmakar on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
